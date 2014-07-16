9058a1af9cd2d5639936f3cf97b2dc0f0caaa144
maiouchy <eng.mai.seif@gmail.com>
2014-07-16 17:12:02 +0200

    CRUD2

--- CRUD2/config/routes.rb
+++ CRUD2/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :teams
   resources :members
 
   # The priority is based upon order of creation: first created -> highest priority.
--- CRUD2/db/schema.rb
+++ CRUD2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714134908) do
+ActiveRecord::Schema.define(version: 20140714144021) do
 
   create_table "members", force: true do |t|
     t.string   "name"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 20140714134908) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "teams", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teams", ["email"], name: "index_teams_on_email", unique: true
+  add_index "teams", ["reset_password_token"], name: "index_teams_on_reset_password_token", unique: true
 
   create_table "usernames", force: true do |t|
     t.string   "group"
